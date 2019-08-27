38a23867d8767efc1981014dbba1019f05313ea7
Janette Chiu <janettechiu@gmail.com>
2019-08-27 22:20:53 +1000

    Add some css

--- src/music/Viewer.js
+++ src/music/Viewer.js
@@ -1,28 +1,35 @@
-import React from "react"
+import React from 'react'
+import './Viewer.css'
 
 class Viewer extends React.Component {
   constructor(props) {
-    super(props);
-    this.ref = React.createRef();
+    super(props)
+    this.ref = React.createRef()
   }
 
   componentDidMount() {
     fetch(
-      "https://raw.githubusercontent.com/music-encoding/sample-encodings/master/MEI_3.0/Music/Complete_examples/Aguado_Walzer_G-major.mei"
+      'https://raw.githubusercontent.com/music-encoding/sample-encodings/master/MEI_3.0/Music/Complete_examples/Aguado_Walzer_G-major.mei'
     )
       .then(serverResponse => serverResponse.text())
       .then(data => {
         console.log(data)
 
-        var vrv = new window.verovio.toolkit()
+        let vrv = new window.verovio.toolkit()
+
+        const options = {
+          scale: 50,
+          adjustPageHeight: true
+        }
+        vrv.setOptions(options);
 
         this.ref.current.innerHTML = vrv.renderData(data, {})
       });
   }
 
   render() {
-    return <div ref={this.ref}>MEI</div>;
+    return <div ref={this.ref}>MEI</div>
   }
 }
 
-export default Viewer;
+export default Viewer
